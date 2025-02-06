326fac5041269c8a2be006b0b2b786de995f66e1
Sondos <Sondosbakr81@gmail.com>
2025-02-07 00:15:23 +0200

    Practical Exam

--- Exam02OOP/PracticalExam.cs
+++ Exam02OOP/PracticalExam.cs
@@ -6,51 +6,52 @@ using System.Threading.Tasks;
 
 namespace Exam02OOP
 {
-    internal class FinalExam : Exam
+    internal class PracticalExam : Exam
     {
-        public FinalExam(int time, int numberOfQuestion, Question[] questions) : base(time, numberOfQuestion, questions)
+        public PracticalExam(int time, int numberOfQuestion, Question[] questions) : base(time, numberOfQuestion, questions)
         {
         }
 
         public override void ShowExam()
         {
-            Console.WriteLine("-------------------------- Final Exam ---------------------------");
-            Dictionary < string, string> questionResult = new Dictionary<string, string>();
-            int score = 0;
+            Console.WriteLine("------------------------- Practical Exam ------------------------");
+            Dictionary<string, string> questionResult = new Dictionary<string, string>();
 
             if (Questions == null || Questions.Length == 0)
             {
                 Console.WriteLine("No questions available for this exam.");
                 return;
             }
+            int score = 0;
 
-            for (int i = 0; i < Questions?.Length; i++)
+            for (int i = 0; i < Questions.Length; i++)
             {
                 Questions[i].Display();
 
-                string questionBody= Questions[i]?.Body ?? "Unknown Question";
+                string questionBody = Questions[i]?.Body ?? "Unknown Question";
                 string rightAnswer = Questions[i].CorrectAnswer?.AnswerText ?? "No Correct Answer";
                 questionResult[questionBody] = rightAnswer;
-                
+
                 bool isParsed;
                 int userAnswer;
                 do
                 {
-                    Console.Write("Please Enter Your Answer : ");
+                    Console.Write($"Please Enter Your Answer: ");
                     isParsed = int.TryParse(Console.ReadLine(), out userAnswer);
                 } while (!isParsed);
 
                 if(userAnswer == Questions[i]?.CorrectAnswer?.AnswerId)
+                {
                     score += Questions[i].Mark;
+                }
             }
-
             foreach (var item in questionResult)
             {
                 Console.WriteLine($"Question: {item.Key}");
                 Console.WriteLine($"Correct Answer {item.Value}");
                 Console.WriteLine("-------------------------------------");
             }
-            Console.WriteLine($"Your Grade Is {score}");
+
         }
     }
 }
